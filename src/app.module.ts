b326ca9c980642bfad07f20b85fe646754d052d0
Kyoung Yeob <kkyub1313@gmail.com>
2024-06-10 23:56:08 +0900

    Section8 SQL & DOCKER -2
    + TypeORM 사용해보기 1강

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    TypeOrmModule.forRoot({
+      // 데이터베이스 타입
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1234',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+    }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
