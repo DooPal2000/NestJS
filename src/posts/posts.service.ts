a35d4fcee6d9589ee754b420d7aa02c57921636f
Kyoung Yeob <kkyub1313@gmail.com>
2024-06-12 01:16:54 +0900

    Section9. Delete 함수

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -3,41 +3,6 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { PostModel } from './entities/posts.entity';
 
-// export interface PostModel {
-//     id: number;
-//     author: string;
-//     title: string;
-//     content: string;
-//     likeCount: number;
-//     commentCount: number;
-// }
-
-let posts: PostModel[] = [
-    {
-        id: 1,
-        author: 'against_the_current',
-        title: 'atc',
-        content: '공연 준비 중인 atc',
-        likeCount: 10000,
-        commentCount: 99999
-    },
-    {
-        id: 2,
-        author: 'against_the_current',
-        title: 'chrissy',
-        content: '공연 준비 중인 chrissy',
-        likeCount: 10000,
-        commentCount: 99999
-    },
-    {
-        id: 3,
-        author: 'blackpink_official',
-        title: '로제',
-        content: '공연 준비 중인 로제',
-        likeCount: 10000,
-        commentCount: 99999
-    }
-]
 
 
 @Injectable() // 이 어노테이션 외에도 module.ts 에도 등록해야 합니다. 2가지가 되어야 DI 완료
@@ -106,13 +71,16 @@ export class PostsService {
         return newPost;
     }
 
-    deletePost(postId: number) {
-        const post = posts.find((post) => post.id === postId);
+    async deletePost(postId: number) {
+        const post = await this.postsRepository.findOne({
+            where:{
+                id: postId,
+            }
+        });
         if (!post) {
             throw new NotFoundException();
         }
-        posts = posts.filter(post => post.id !== postId);
-
+        await this.postsRepository.delete(postId);
         return postId;
     }
 }
