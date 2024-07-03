d5ffd5fece72f7efa1bdf7a6fea99e413090e87d
Kyoung Yeob <kkyub1313@gmail.com>
2024-07-04 01:24:41 +0900

    Section13. 인증 - 회원가입, 로그인 엔드포인트 만들기(1)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,32 +5,56 @@ import { BasicTokenGuard } from './guard/basic-token.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
 
   @Post('login/email')
-  @UseGuards(BasicTokenGuard)
-  postLoginEmail(
-    @Headers('authorization') rawToken: string,
-  ){
-    // email:password -> base64
-    // cxvjkzxkclvjdifasd -> email:password
-    const token = this.authService.extractTokenFromHeader(rawToken, false);
-
-    const credentials = this.authService.decodeBasicToken(token);
-
-    return this.authService.loginWithEmail(credentials);
+  loginEmail(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    return this.authService.loginWithEmail({
+      email,
+      password,
+    });
   }
 
-  @Post('reigiser/email')
-  postRegisterEmail(@Body('nickname') nickname: string,
-    @Body('email') email:string,
-    @Body('password', new MaxLengthPipe(8), new MinLengthPipe(3)) password: string,
-  ){
+  // @Post('login/email')
+  // @UseGuards(BasicTokenGuard)
+  // postLoginEmail(
+  //   @Headers('authorization') rawToken: string,
+  // ){
+  //   // email:password -> base64
+  //   // cxvjkzxkclvjdifasd -> email:password
+  //   const token = this.authService.extractTokenFromHeader(rawToken, false);
+
+  //   const credentials = this.authService.decodeBasicToken(token);
+
+  //   return this.authService.loginWithEmail(credentials);
+  // }
+
+  
+  @Post('register/email')
+  registerEmail(@Body('nickname') nickname: string,
+  @Body('email') email: string,
+  @Body('password') password: string){
     return this.authService.registerWithEmail({
       nickname,
       email,
       password,
     })
-  }  
+  }
+
+  // @Post('reigiser/email')
+  // postRegisterEmail(@Body('nickname') nickname: string,
+  //   @Body('email') email: string,
+  //   @Body('password', new MaxLengthPipe(8), new MinLengthPipe(3)) password: string,
+  // ) {
+  //   return this.authService.registerWithEmail({
+  //     nickname,
+  //     email,
+  //     password,
+  //   })
+  // }
+  
 }
