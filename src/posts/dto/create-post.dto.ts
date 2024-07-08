55eaec9b7d050573bef7bf0b8db7e4ff796aac1b
Kyoung Yeob <kkyub1313@gmail.com>
2024-07-08 23:31:08 +0900

    Section20. Class Validator, PickType 활용

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,13 +1,11 @@
 import { IsString } from "class-validator";
+import { PostsModel } from "../entities/posts.entity";
+import { PickType } from "@nestjs/mapped-types";
 
-export class CreatePostDto{
-    @IsString({
-        message: 'title은 string 타입을 입력해줘야 합니다.'
-    })
-    title:string;
+export class CreatePostDto extends PickType(PostsModel, ['title', 'content']) { }
 
-    @IsString({
-        message: 'content는 string 타입을 입력해줘야 합니다.'
-    })
-    content: string;
-}
+// Pick은 type을 입력해야 하는데, 여기엔 값이 들어가있다?
+// 그래서 Pick 과 같은 기능을 해주는데, 값을 반환해주는 기능을 만들어뒀다.
+
+// Pick, Omit, Partial -> 타입반환
+// PickType, OmitType, ParitalType -> 값 반환(extends 가능)
