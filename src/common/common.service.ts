ac58e92df7375f4c90f2710e72ad27a9ee58de0d
Kyoung Yeob <kkyub1313@gmail.com>
2024-07-22 00:09:51 +0900

    Section24. Pagination심화 - 작업할 Pagination 로직 정리하기

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -11,8 +11,72 @@ export class CommonService {
         overrideFindOptions: FindManyOptions<T> = {},
         path: string,
     ) {
-
+        if (dto.page) {
+            this.pagePaginate(
+                dto,
+                repository,
+                overrideFindOptions
+            )
+        } else {
+            return this.cursorPaginate(
+                dto,
+                repository,
+                overrideFindOptions,
+                path,
+            )
+        }
 
     }
 
+    private async pagePaginate<T extends BaseModel>(
+        dto: BasePaginationDto,
+        repository: Repository<T>,
+        overrideFindOptions: FindManyOptions<T> = {},
+    ) {
+
+    }
+    private async cursorPaginate<T extends BaseModel>(
+        dto: BasePaginationDto,
+        repository: Repository<T>,
+        overrideFindOptions: FindManyOptions<T> = {},
+        path: string,
+    ) {
+        /**
+         * where__likeCount__more_than
+         * 
+         * where__title__ilike
+         */
+
+    }
+    
+    private composeFindOptions()<T extends BaseModel>(
+        dto: BasePaginationDto,
+    ) : FindManyOptions<T>{
+        /**
+         * where,
+         * order,
+         * take,
+         * skip -> page 기반일때만
+         */
+
+        /**
+         * DTO 의 현재 생긴 구조는 아래와 같음
+         * {
+         *  where__id__more_than : 1 
+         *  order__createdAt : 'ASC'
+         * }
+         * 
+         * 현재는 where__id__more_than / where__id__less_than 에 해당하는 where 필터 사용중이지만
+         * 나중에 where__likeCount__more_than 이나 where__title__ilike 등 추가 필터를 넣고싶을때
+         * 모든 where 필터들을 자동으로 파싱 할 수 있을만한 기능을 제공한다.
+         * 
+         * 1) where로 시작한다면 필터 로직을 적용한다.
+         * 2) order로 시작한다면 정렬 로직을 적용한다.
+         * 3) 필터 로직을 적용한다면 '__' 기준으로 split 했을 때 split 했을 때 3개의 값으로 나뉘는지
+         *  2개의 값으로 나뉘는지 확인한다.
+         *  3-1) 3개의 값으로 나뉜다면, FILTER_MAPPER에서 해당되는 operator  함수를 찾아서 적용한다.
+         *  3-2) 2개의 값으로 나뉜다면, 정확한 값을 필터하는 것이기 때문에 operator 없이 적용한다.
+         * 4) order 의 경우, 3-2와 같이 적용한다.
+         */
+    }
 }
