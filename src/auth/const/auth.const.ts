321c0785f9845abdfc2250a3ad7830dffbca1f66
Kyoung Yeob <kkyub1313@gmail.com>
2024-07-29 00:14:28 +0900

    Section25. Config 모듈 사용하기 - 환경변수 적용하기 1

--- src/auth/const/auth.const.ts
+++
@@ -1,2 +0,0 @@
-export const JWT_SECRET = 'kyoungyeob';
-export const HASH_ROUNDS = 10;
---
+++ src/common/const/env-keys.const.ts
@@ -0,0 +1,19 @@
+//서버 프로토콜 -> HTTP /HTTPS
+export const ENV_PROTOCOL_KEY = 'PROTOCOL';
+
+//서버 호스트 -> localhost:3000
+export const ENV_HOST_KEY = 'HOST';
+
+//JWT 토큰 시크릿 -> kyoungyeob
+export const ENV_JWT_SECRET_KEY = 'JWT_SECRET';
+//JWT 토큰 해시 라운드 수 -> 10
+export const ENV_HASH_ROUNDS_KEY = 'HASH_ROUNDS';
+//데이터베이스 호스트 -> localhost
+export const ENV_DB_HOST_KEY = 'DB_HOST';
+//데이터베이스 포트 -> 5432
+export const ENV_DB_PORT_KEY = 'DB_PORT';
+
+export const ENV_DB_USERNAME = 'DB_USERNAME';
+export const ENV_DB_PASSWORD_KEY = 'DB_PASSWORD';
+export const ENV_DB_DATABASE_KEY = 'DB_DATABASE';
+
