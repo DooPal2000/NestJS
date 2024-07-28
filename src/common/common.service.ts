1a8b738e88026c41aa434df54897e74da28c6b99
Kyoung Yeob <kkyub1313@gmail.com>
2024-07-28 16:27:39 +0900

    Section24. Pagination심화 - return 누락 수정

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -14,8 +14,10 @@ export class CommonService {
         overrideFindOptions: FindManyOptions<T> = {},
         path: string,
     ) {
+        console.log('CommonService paginate dto:', dto);
+
         if (dto.page) {
-            this.pagePaginate(
+            return this.pagePaginate(
                 dto,
                 repository,
                 overrideFindOptions
@@ -37,15 +39,18 @@ export class CommonService {
         overrideFindOptions: FindManyOptions<T> = {},
     ) {
         const findOptions = this.composeFindOptions<T>(dto);
+        console.log('pagePaginate findOptions:', findOptions);
+
         const [data, count] = await repository.findAndCount({
             ...findOptions,
             ...overrideFindOptions,
         });
-        const results = await repository.find({
-            ...findOptions,
-            ...overrideFindOptions,
-        });
 
+
+        return {
+            data: data,
+            total: count,
+        }
     }
     private async cursorPaginate<T extends BaseModel>(
         dto: BasePaginationDto,
@@ -66,7 +71,7 @@ export class CommonService {
         });
 
         const lastItem = results.length > 0 && results.length === dto.take ? results[results.length - 1] : null;
-        const nextUrl = lastItem && new URL(`${PROTOCOL}://${HOST}/posts`);
+        const nextUrl = lastItem && new URL(`${PROTOCOL}://${HOST}/${path}`);
         if (nextUrl) {
             /**
              * dto의 키값들을 루핑하면서
@@ -91,9 +96,11 @@ export class CommonService {
             nextUrl.searchParams.append(key, lastItem.id.toString())
         }
 
+        console.log('curSorPagination result:', { results });
+
         return {
             data: results,
-            cursor:{
+            cursor: {
                 after: lastItem?.id ?? null,
             },
             count: results.length,
@@ -154,7 +161,7 @@ export class CommonService {
             } else if (key.startsWith('order__')) {
                 order = {
                     ...order,
-                    ...this.parseOrderFilter(key, value),
+                    ...this.parseWhereFilter(key, value),
                 }
             }
         }
@@ -167,12 +174,12 @@ export class CommonService {
         };
     }
     private parseWhereFilter<T extends BaseModel>(key: string, value: any):
-        FindOptionsWhere<T> {
+        FindOptionsWhere<T> | FindOptionsOrder<T> {
         const options: FindOptionsWhere<T> = {};
         const split = key.split('__');
         if (split.length !== 2 && split.length !== 3) {
-            throw new BadRequestException();
-            `where 필터는 '__'로 split 했을때 길이가 2 or 3 이어야 합니다. 문제되는 키값 : ${key}`
+            throw new BadRequestException(`where 필터는 '__'로 split 했을때 길이가 2 or 3 이어야 합니다. 문제되는 키값 : ${key}`);
+
         }
         if (split.length === 2) {
             // [where, id]
@@ -209,8 +216,12 @@ export class CommonService {
             // }else{
             //     options[field] = FILTER_MAPPER[operator](value);
             // }
+            if (operator === 'i_like') {
+                options[field] = FILTER_MAPPER[operator](`%${value}%`);
 
-            options[field] = FILTER_MAPPER[operator](value);
+            } else {
+                options[field] = FILTER_MAPPER[operator](value);
+            }
 
         }
         return options;
