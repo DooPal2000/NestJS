d043377197549bc104d6d565a4cc64bfcc4783bf
Kyoung Yeob <kkyub1313@gmail.com>
2024-07-31 00:08:50 +0900

    Section28. 파일 업로드 - 선 업로드 방식

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -26,37 +26,6 @@ import { v4 as uuid } from 'uuid';
     AuthModule,
     UsersModule,
     CommonModule,
-    MulterModule.register({
-      limits: {
-        // 바이트 단위로 입력 10MB
-        fileSize: 10000000,
-      },
-      fileFilter: (req, file, cb) => {
-        /**
-         * cb(에러, boolean)
-         * 
-         * 첫번째 파라미터는 에러가 있을 경우 에러 정보를 넣어준다.
-         * 두번째 파라미터는 파일을 받을지 말지 boolean을 넣어준다.
-         */
-        // xxx.jpg -> .jpg
-        const ext = extname(file.originalname);
-        if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
-          return cb(
-            new BadRequestException('jpg, jpeg, png 파일만 업로드 가능합니다.'),
-            false,
-          );
-        }
-        return cb(null, true);
-      },
-      storage: multer.diskStorage({
-        destination: function (req, res, cb) {
-          cb(null, POST_IMAGE_PATH);
-        },
-        filename: function (req, file, cb) {
-          cb(null, `${uuid()}${extname(file.originalname)}`)
-        }
-      }),
-    }),
     JwtModule.register({}),
   ],
   controllers: [PostsController],
