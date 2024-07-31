94b1f98dbacf5cda4462bdf85298fbd2dd23c062
Kyoung Yeob <kkyub1313@gmail.com>
2024-07-31 10:22:29 +0900

    Section29. Transaction - ImageModel 생성로직1

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -3,13 +3,15 @@ import { PostsModel } from "../entities/posts.entity";
 import { PickType } from "@nestjs/mapped-types";
 
 export class CreatePostDto extends PickType(PostsModel, ['title', 'content']) {
-    @IsString()
+    @IsString({
+        each: true,
+    })
     @IsOptional()
-    image?: string;
+    images: string[] = [];
 }
 
 // Pick은 type을 입력해야 하는데, 여기엔 type 이 아닌 value(값)이 들어가있다?
 // 그래서 Pick 과 같은 기능을 해주는데, 값을 반환해주는 기능을 만들어뒀다.
 
 // Pick, Omit, Partial -> 타입반환
-// PickType, OmitType, ParitalType -> 값 반환(extends 가능)
+// PickType, OmitType, ParitalType -> 값 반환(extends 가능) 
