c077173481d621db756ab7c7393958fd9c2bad1a
Kyoung Yeob <kkyub1313@gmail.com>
2024-07-31 10:14:12 +0900

    Section29. Transaction - ImageModel 만들기

--- src/posts/entities/posts.entity.ts
+++ src/posts/entities/posts.entity.ts
@@ -3,9 +3,10 @@ import { IsString } from "class-validator";
 import { join } from "path";
 import { POST_PUBLIC_IMAGE_PATH } from "src/common/const/path.const";
 import { BaseModel } from "src/common/entity/base.entity";
+import { ImageModel } from "src/common/entity/image.entity";
 import { stringValidationMessage } from "src/common/validation-message/string-validation.message";
 import { UsersModel } from "src/users/entities/users.entity";
-import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity()
 export class PostsModel extends BaseModel {
@@ -28,11 +29,11 @@ export class PostsModel extends BaseModel {
     })
     content: string;
 
-    @Column({
-        nullable: true,
-    })
-    @Transform(({value})=> value && `/${join(POST_PUBLIC_IMAGE_PATH, value)}`)
-    image?: string;
+    // @Column({
+    //     nullable: true,
+    // })
+    // @Transform(({value})=> value && `/${join(POST_PUBLIC_IMAGE_PATH, value)}`)
+    // image?: string;
 
     @Column()
     likeCount: number;
@@ -40,4 +41,6 @@ export class PostsModel extends BaseModel {
     @Column()
     commentCount: number;
 
+    @OneToMany((type) => ImageModel, (image) => image.post)
+    images: ImageModel[];
 }
