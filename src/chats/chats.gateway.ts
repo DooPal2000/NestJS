9d6c86b3856189b26eb931e0adc1317c7a20b9b2
Kyoung Yeob <kkyub1313@gmail.com>
2024-08-24 16:20:07 +0900

    Section35. SocketIO Validation Pipe

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -5,6 +5,7 @@ import { ChatsService } from "./chats.service";
 import { EnterChatDto } from "src/common/dto/enter-chat.dto";
 import { CreateMessageDto } from "./messages/dto/create-messages.dto";
 import { ChatsMessagesService } from "./messages/messages.service";
+import { UsePipes, ValidationPipe } from "@nestjs/common";
 
 @WebSocketGateway({
     // ws://localhost:3000/chats
@@ -23,6 +24,14 @@ export class ChatsGateway implements OnGatewayConnection {
         console.log(`on connect called: ${socket.id}`);
     }
 
+    @UsePipes(new ValidationPipe({
+        transform: true,
+        transformOptions: {
+            enableImplicitConversion: true,
+        },
+        whitelist: true,
+        forbidNonWhitelisted: true,
+    }))    
     @SubscribeMessage('create_chat')
     async createChat(
         @MessageBody() data: CreateChatDto,
